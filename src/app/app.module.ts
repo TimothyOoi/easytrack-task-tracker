f01ce796928633b82624281cc6ce9b49ff256dca
Timothy Ooi <timothy.ooi@basf.com>
2025-04-10 12:19:12 +0800

    added shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 
 @NgModule({
@@ -16,12 +13,8 @@ import { NewTaskComponent } from './tasks/new-task/new-task.component';
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, SharedModule, TasksModule],
 })
 export class AppModule {}
